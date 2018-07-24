d591604d3154232779d184dfa1f67a5c3c703972
test191197 <41562334+test191197@users.noreply.github.com>
2018-07-24 14:36:16 +0700

    aaa

--- src/javaapplication30/JavaApplication30.java
+++ src/javaapplication30/JavaApplication30.java
@@ -12,9 +12,8 @@ package javaapplication30;
 public class JavaApplication30 {
 
     public static void main(String args[]) {
-        System.out.println("hello worldasadas");
-        System.out.println("hello worldasadas");
-        System.out.println("hello worldasadas");
-        System.out.println("hello worldasadas");
+        System.out.println("helloasdasdasdsdds");
+        System.out.println("helloasdasdasdsdds");
+        System.out.println("helloasdasdasdsdds");
     }
 }
